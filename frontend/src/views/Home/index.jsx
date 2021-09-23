3044f8669d8afd4e8cbeaac8ca8e7bf924db7093
Alex <alexmunoz91@gmail.com>
2021-09-23 13:48:39 +0200

    Add GnomesList component

---
+++ frontend/src/views/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GnomesList from '../../components/GnomesList';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <GnomesList />
+    </main>
+  );
+}
