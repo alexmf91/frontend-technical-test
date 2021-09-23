10ca18ebd6aa7fd6b23a75482214295c7fedbe13
Alex <alexmunoz91@gmail.com>
2021-09-23 20:12:38 +0200

    Add avatar comp. and manage 0 length properties

--- frontend/src/views/Details/index.jsx
+++ frontend/src/views/Details/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import loadGnomes from '../../redux/actions/creators/gnomes';
+import Avatar from '../../components/Avatar';
 
 export default function Home() {
   const gnomesList = useSelector(({ gnomes }) => gnomes);
@@ -15,28 +16,38 @@ export default function Home() {
   return (
     <main>
       <h1>Details Page</h1>
-      <img src={currentGnome.thumbnail} alt="gnome-img" />
-      <h2>{currentGnome.name}</h2>
+      <Avatar
+        picture={currentGnome?.thumbnail}
+        width={260}
+        height={260}
+      />
+      <h2>{currentGnome?.name}</h2>
       <p>
         Age:
         {' '}
         <span>
-          {currentGnome.age}
+          {currentGnome?.age}
         </span>
       </p>
-      <ul>
-        {currentGnome.friends?.map((friend) => (
-          <li>
-            {friend}
-          </li>
-        ))}
+      <ul className="friends">
+        <h3>Friends</h3>
+        {currentGnome.friends?.length === 0
+          ? <p>{`${currentGnome.name} have no friends yet`}</p>
+          : currentGnome?.friends?.map((friend) => (
+            <li key={friend}>
+              {friend}
+            </li>
+          ))}
       </ul>
-      <ul>
-        {currentGnome.professions?.map((profession) => (
-          <li>
-            {profession}
-          </li>
-        ))}
+      <ul className="professions">
+        <h3>Professions</h3>
+        {currentGnome.professions?.length === 0
+          ? <p>{`${currentGnome.name} have no profession yet`}</p>
+          : currentGnome?.professions?.map((profession) => (
+            <li key={profession}>
+              {profession}
+            </li>
+          ))}
       </ul>
     </main>
   );
