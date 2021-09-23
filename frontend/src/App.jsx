974522a1263dfa435c70f4dcd736be8dc471ad19
Alex <alexmunoz91@gmail.com>
2021-09-23 13:45:27 +0200

    Add home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Home from './views/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>helloworld</h1>
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
