6b404e5b3b105eab59d597e762506ab34198a029
Alex <alexmunoz91@gmail.com>
2021-09-23 23:58:37 +0200

    Add filter funcionality and Avatar component

--- frontend/src/components/GnomesList/index.jsx
+++ frontend/src/components/GnomesList/index.jsx
@@ -1,26 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import loadGnomes from '../../redux/actions/creators/gnomes';
+import Avatar from '../Avatar';
+import './GnomesList.scss';
 
 export default function GnomesList() {
   const gnomesList = useSelector(({ gnomes }) => gnomes);
   const dispatch = useDispatch();
+  const [filteredGnomesList, setFilteredGnomesList] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  function handleChange(input) {
+    setInputValue(input.target.value);
+  }
   useEffect(() => {
     dispatch(loadGnomes());
   }, []);
+  useEffect(() => {
+    setFilteredGnomesList(gnomesList
+      .filter((gnome) => gnome.name.toLowerCase().includes(inputValue.toLowerCase())));
+  }, [inputValue, gnomesList]);
+
   return (
-    <>
+    <main className="gnomes-list-container">
       <h1>Gnomes List</h1>
-      <ul>
-        {gnomesList?.map((gnome) => (
-          <li key={gnome.id}>
-            <Link to={`${gnome.id}`}>
-              {gnome.name}
-            </Link>
+      <input type="text" name="name" onChange={handleChange} placeholder="Find a gnome" />
+      <ul className="gnomes-list-container__gnomes-list">
+        {filteredGnomesList?.map((gnome) => (
+          <li className="gnomes-list__gnome" key={`${gnome.id}`}>
+            <Avatar
+              picture={gnome?.thumbnail}
+              width={60}
+              height={60}
+            />
+            <div className="gnome__details">
+              <span>
+                {gnome?.name}
+              </span>
+              <Link to={`${gnome.id}`}>
+                <button type="button">Details</button>
+              </Link>
+            </div>
           </li>
         ))}
       </ul>
-    </>
+    </main>
   );
 }
