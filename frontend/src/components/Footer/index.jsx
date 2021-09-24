cca35272339477a510a14eefc14221598f2402e0
Alex <alexmunoz91@gmail.com>
2021-09-24 04:04:06 +0200

    Add footer component

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <footer>
+      <span>SocialGnomes.Net 2021 © Made with ♡ by Alex Muñoz</span>
+    </footer>
+  );
+}
