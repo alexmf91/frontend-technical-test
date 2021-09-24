7b8f3060ee2987c3f60f1e929f76b0e691f578a7
Alex <alexmunoz91@gmail.com>
2021-09-24 04:06:18 +0200

    Import homeBanner component

--- frontend/src/views/Home/index.jsx
+++ frontend/src/views/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import HomeBanner from '../../components/HomeBanner';
 import GnomesList from '../../components/GnomesList';
 import './Home.scss';
 
 export default function Home() {
   return (
     <main className="home-page">
+      <HomeBanner />
       <GnomesList />
     </main>
   );
