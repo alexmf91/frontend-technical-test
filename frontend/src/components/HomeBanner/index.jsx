0fa78feb0883a6b78df487f8ee6cdb9486af37f3
Alex <alexmunoz91@gmail.com>
2021-09-24 12:37:26 +0200

    Add an exclamation

--- frontend/src/components/HomeBanner/index.jsx
+++ frontend/src/components/HomeBanner/index.jsx
@@ -5,7 +5,7 @@ import './HomeBanner.scss';
 export default function HomeBanner() {
   return (
     <section className="home-banner">
-      <p>Hello heroes! Here you can find the Brastlewark gnomes that you want to help</p>
+      <p>Hello heroes! Here you can find the Brastlewark gnomes that you want to help!</p>
     </section>
   );
 }
