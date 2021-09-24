96732284789fdca4f67204828a659ab4eb1bac05
Alex <alexmunoz91@gmail.com>
2021-09-24 12:38:42 +0200

    Add app test

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,9 +1,12 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { render, screen } from './utils/test.utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Given an App component', () => {
+  describe('When the component is rendered', () => {
+    test('Then heroes text should be in the document', () => {
+      render(<App />);
+      expect(screen.getByText(/heroes/i)).toBeInTheDocument();
+    });
+  });
 });
