8bfbc033e3cdfbb20a2a30ab26c98d5ff7a9c48d
Alex <alexmunoz91@gmail.com>
2021-09-24 04:02:56 +0200

    Add homeBanner component

---
+++ frontend/src/components/HomeBanner/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './HomeBanner.scss';
+
+export default function HomeBanner() {
+  return (
+    <section className="home-banner">
+      <p>Hello heroes! Here you can find the Brastlewark gnomes that you want to help</p>
+    </section>
+  );
+}
